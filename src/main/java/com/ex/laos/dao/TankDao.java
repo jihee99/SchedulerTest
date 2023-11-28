aa3da38af4e919426229c73d84e2a071cc4ec90d
손지희 <wlwlhee@ejbt.co.kr>
2023-11-28 14:44:23 +0900

    Added methods and SQL statements for inserting dam tank prediction history, summary, and result overwrite.
    Related to : #2

--- src/main/java/com/ex/laos/dao/TankDao.java
+++ src/main/java/com/ex/laos/dao/TankDao.java
@@ -5,9 +5,16 @@ import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import com.ex.laos.dto.tank.PredictionSummaryDto;
+
 @Mapper
 public interface TankDao {
 
 	Map<String, BigDecimal> getTankInputParametersByDamId(String damId);
 
+	void insertDamTankPredictionHistory(PredictionSummaryDto psd);
+
+	void insertDamTankPredictionSummary(PredictionSummaryDto psd);
+
+	void insertDamTankPredictionResultOverwrite(PredictionSummaryDto psd);
 }
