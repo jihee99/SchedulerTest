75a54b8af8c52eb22d954a802c1e301361ad7709
손지희 <wlwlhee@ejbt.co.kr>
2023-12-04 14:48:50 +0900

    Title : 회원정보 업데이트 파라미터 변경
    - 비밀번호 변경 히스토리 업데이트 파라미터 수정
    
    Related to : #3

--- src/main/java/com/ex/laos/common/service/CustomUserDetailsService.java
+++ src/main/java/com/ex/laos/common/service/CustomUserDetailsService.java
@@ -1,5 +1,9 @@
 package com.ex.laos.common.service;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 import org.springframework.security.core.userdetails.*;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -37,6 +41,31 @@ public class CustomUserDetailsService implements UserDetailsService {
 			throw new UsernameNotFoundException("User not found with username : " + username);
 		}
 		return new CustomUserDetails(member);
+	}
+
+	public boolean isPasswordValid(String username, String rawPassword) {
+		MemberDto member = memberDao.findByUserId(username);
+		if (member != null) {
+			String encodedPassword = member.getPswd();
+			return passwordEncoder.matches(rawPassword, encodedPassword);
+		}
+		return false;
+	}
+
+	@Transactional
+	public void updatePassword(String username, String rawPassword){
+
+		Map<String, String> userdata = new HashMap<>();
+		userdata.put("username", username);
+		userdata.put("pswdChgId", UUID.randomUUID().toString());
+
+		// 1. 비밀번호 변경 이력 테이블에 insert
+		memberDao.insertPasswordUpdateHistory(userdata);
+
+		// 2. 사용자 정보 update
+		String updatePwd = passwordEncoder.encode(rawPassword);
+		System.out.println(updatePwd);
+		memberDao.updatePasswordByUsername(username, updatePwd);
 	}
 
 
